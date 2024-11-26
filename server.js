92e13f1188b80f88e39fb2d2a14f9a572b00b820
Elnegocios27 <maximogaspar777@outlook.com>
2024-11-26 18:29:58 -0300

    mongoose

--- server.js
+++ server.js
@@ -7,16 +7,19 @@ dotenv.config();
 
 const app = express ();
 const PORT = 8080;
-let DB = process.env.DB
+const DB = process.env.DB
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api/post', postRoutes);
 
 mongoose.connect(DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+    ssl: true,   // Asegura que SSL esté habilitado
+    sslValidate: true,  // Opcional, asegura que el certificado SSL sea validado
+    sslCA: [fs.readFileSync('/path/to/ca.pem')]  // Opcional, si necesitas especificar un archivo de autoridad de certificación (CA), normalmente no es necesario para MongoDB Atlas.
+})
+    .then(() => console.log('Connected to MongoDB successfully'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 app.listen(PORT, () =>{
 console.log(`Server listen on http://localhost:${PORT}/api/post`)
